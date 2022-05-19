de4de527ae40c9c0e24271e5ebe5ce1f3cd7d4d3
minuk0506 <leeminuk9238@daum.net>
2022-05-19 15:38:38 +0900

    0519/3:38

--- js/academy-info.js
+++ js/academy-info.js
@@ -1,17 +1,19 @@
-const map_button = document.querySelector(".map"); // map버튼
+const map_button = document.querySelectorAll(".map-btn"); // map버튼
 const aca_wrapper = document.querySelector(".section"); // info
 const put_in_btn = document.querySelector(".put-in-btn");
 
-map_button.addEventListener("click", () => {
-  console.log("hi");
-  // map누르면 info 나오게
-  console.log(basket_wrapper.className);
-  if (basket_wrapper.className === "wrapper click") {
-    return;
-  } else if (basket_wrapper.className === "wrapper") {
-    aca_wrapper.classList.toggle("click");
-  }
-});
+for (let btn of map_button) {
+  btn.addEventListener("click", () => {
+    console.log("hi");
+    // map누르면 info 나오게
+    console.log(basket_wrapper.className);
+    if (basket_wrapper.className === "wrapper click") {
+      return;
+    } else if (basket_wrapper.className === "wrapper") {
+      aca_wrapper.classList.toggle("click");
+    }
+  });
+}
 
 put_in_btn.addEventListener("click", () => {
   aca_wrapper.classList.remove("click");
